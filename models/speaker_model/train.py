592928bd9f4f73bace1a8c8d7ad7ec1965ab116d
Xinyi Chen <1159736359@qq.com>
2024-04-27 19:46:54 -0700

    fix

--- models/speaker_model/train.py
+++ models/speaker_model/train.py
@@ -21,14 +21,14 @@ parser.add_argument('--special_word', type=int, default=3,
                     help='default special words include: padding, EOS, EOT.')
 
 parser.add_argument('--fine_tuning', action='store_true')
-parser.add_argument('--fine_tunine_model', type=str, default='model')
+parser.add_argument('--fine_tuning_model', type=str, default='model10')
 
 parser.add_argument('--PersonaNum', type=int, default=2)
 parser.add_argument('--SpeakerMode', action='store_true')
 parser.add_argument('--AddresseeMode', action='store_true')
 
 parser.add_argument("--batch_size", type=int, default=256)
-parser.add_argument("--source_max_length", type=int, default=50)
+parser.add_argument("--source_max_length", type=int, default=50)        # Source text length
 parser.add_argument("--target_max_length", type=int, default=50)
 parser.add_argument("--max_iter", type=int, default=10)
 
@@ -36,12 +36,13 @@ parser.add_argument("--dimension", type=int, default=512)
 parser.add_argument("--layers", type=int, default=4)
 parser.add_argument("--init_weight", type=float, default=0.1)
 
-parser.add_argument("--alpha", type=int, default=1)
-parser.add_argument("--start_halve", type=int, default=6)
+parser.add_argument("--alpha", type=int, default=1)         # initial learning rate
+parser.add_argument("--start_halve", type=int, default=6)       # learning rate halve
 parser.add_argument("--thres", type=int, default=5)
 parser.add_argument("--dropout", type=float, default=0.2)
 
 args = parser.parse_args()
+
 print(args)
 print()
 
