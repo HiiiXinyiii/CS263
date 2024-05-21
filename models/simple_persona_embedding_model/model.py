ff9d11e5f3341bf54e93e4fbe7bef51462f0b61e
Xinyi Chen <1159736359@qq.com>
2024-05-20 21:22:29 -0700

    create folder if not existed during saving

--- models/simple_persona_embedding_model/model.py
+++ models/simple_persona_embedding_model/model.py
@@ -1,3 +1,4 @@
+import os
 import json
 import torch
 from torch import nn
@@ -74,6 +75,10 @@ class PersonaChatbot(nn.Module):
 
     def save_model(self, file_path="savings/model.cpt"):
         # torch.save(self.state_dict(), file_path)
+
+        folder_path = os.path.dirname(file_path)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
         torch.save(self, file_path)
 
 
