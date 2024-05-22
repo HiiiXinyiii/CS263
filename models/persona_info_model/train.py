286ce6e5231458ec8d7c089f54b3bd90c6bd8c1f
Xinyi Chen <1159736359@qq.com>
2024-05-22 00:02:56 -0700

    update

--- models/persona_info_model/train.py
+++ models/persona_info_model/train.py
@@ -1,49 +1,56 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments
+import json
+import os
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments, TextDataset, DataCollatorForLanguageModeling
 from process_data import *
 from model import Persona
 
 
-# Persona Information
-persona_info = "Elaine is a female student at UCLA from China. "
-# Base model
-base_model = GPT2LMHeadModel.from_pretrained('gpt2')
-# Tokenizer
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-# Data
-encoded_dialogues = [incorporate_persona_info_into_dialogue(dialogue, tokenizer)
-                     for dialogue in read_data()["dialogue"]]
-dataset = Dataset.from_dict({"dialogues": encoded_dialogues})
-# Define model
-params = {
-    "persona_info": persona_info,
-    "tokenizer": tokenizer,
-    "base_model": base_model
-}
-model = Persona(params)
+def train():
+    # Get conversation data
+    conversation = combine(mode="train", json_path="data/train_spc_dataset.json", save_path="savings/processed_conversations.txt")
+
+    # Load model
+    model = GPT2LMHeadModel.from_pretrained("gpt2")
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+    # Load data
+    train_dataset = TextDataset(
+            tokenizer=tokenizer,
+            file_path="savings/processed_conversations.txt",
+            block_size=512
+    )
+
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer,
+        mlm=False,
+    )
+
+    # Params
+    training_args = TrainingArguments(
+        output_dir="savings",
+        overwrite_output_dir=True,
+        num_train_epochs=300,
+        per_device_train_batch_size=4,
+        save_steps=1000,
+        save_total_limit=2,
+    )
+
+    # Create trainer instance
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=train_dataset,
+    )
+
+    # train model
+    trainer.train()
+
+    # Save fine-tuned model
+    model.save_pretrained('savings/finetuned_gpt2')
+    tokenizer.save_pretrained('savings/finetuned_gpt2')
 
 
-# Define training arguments
-training_args = TrainingArguments(
-    output_dir='./results',          # The output directory for the model predictions and checkpoints
-    num_train_epochs=3,              # Total number of training epochs
-    per_device_train_batch_size=4,   # Batch size for training
-    warmup_steps=500,                # Number of warmup steps for learning rate scheduler
-    weight_decay=0.01,               # Strength of weight decay
-    logging_dir='./logs',            # Directory for storing logs
-)
-
-# Initialize the Trainer
-trainer = Trainer(
-    model=model.base_model,
-    args=training_args,
-    train_dataset=dataset['dialogues']
-)
-
-
-# Train the model
-trainer.train()
-
-
-# Save the model and the tokenizer
-model.base_model.save_pretrained('./savings/elaine_persona_model')
-model.tokenizer.save_pretrained('./savings/elaine_persona_model')
+if __name__ == "__main__":
+    train()
